101e5df9a5304df45c53e185908be060eaffe39b
ParigiJ <135647142+ParigiJ@users.noreply.github.com>
2025-08-14 21:18:36 -0500

    feat: update links to open in a new browser tab

--- src/app/components/ui/ProfessionalCard.jsx
+++ src/app/components/ui/ProfessionalCard.jsx
@@ -6,6 +6,8 @@ const ProfessionalCard = ({ link, src, projectName }) => {
   return (
     <Link
       href={link}
+      target="_blank"
+      rel="noopener noreferrer"
       className="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 
       rounded-2xl p-3 shadow-sm hover:shadow-xl transition-all duration-300 hover:scale-105 md:hover:scale-120 w-full 
       max-w-sm mx-5 hover:cursor-pointer md:flex-shrink-1 md:w-1/3 md:max-w-xs"
@@ -24,13 +26,6 @@ const ProfessionalCard = ({ link, src, projectName }) => {
             className="rounded-lg transition-transform duration-300 w-auto h-auto border-gray-300 dark:border-black border-1"
           />
         </div>
-
-        {/* <Link
-          href={link}
-          className="w-2/4 bg-blue-600 hover:bg-blue-700 text-white font-medium py-3 px-4 rounded-4xl transition-colors duration-200 inline-block text-center text-md"
-        >
-          Visit {projectName}
-        </Link> */}
       </div>
     </Link>
   );
