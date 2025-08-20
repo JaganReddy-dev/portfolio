e9707bd6867a7c5c69fc3cf18d740bb8161b4cb1
ParigiJ <135647142+ParigiJ@users.noreply.github.com>
2025-08-20 10:56:48 -0500

    fix: mobile menu now shows immediately on mobile devices

--- src/app/components/menus/MobileMenu.jsx
+++ src/app/components/menus/MobileMenu.jsx
@@ -2,7 +2,9 @@ const MobileMenu = ({ links, isMenuOpen, isScrolled, onLinkClick }) => {
   return (
     <section
       className={`fixed top-12 border-collapse border-t-0 right-0 w-full shadow-2xl md:w-1/4 h-full z-30 backdrop-filter backdrop-blur-sm bg-white text-black dark:bg-white/60 dark:text-black transform transition-transform duration-300 ease-in-out ${
-        isMenuOpen && isScrolled ? "translate-x-0" : "translate-x-full"
+        isMenuOpen ? "translate-x-0" : "translate-x-full"
+      } ${
+        isMenuOpen && isScrolled ? "md:translate-x-0" : "md:translate-x-full"
       }`}
       aria-label="Menu Options"
     >
